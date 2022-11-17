cd246b9823e9a09259b896165e97c4907d262443
thetlwinlwin <minthantlwin8@gmail.com>
2022-11-17 20:27:12 +0630

    added sorting btn

--- lib/features/shared/widgets/app_bar.dart
+++ lib/features/shared/widgets/app_bar.dart
@@ -5,14 +5,17 @@ import 'settings_btn.dart';
 
 class MyCustomAppBar extends AppBar {
   final String? titleText;
+  final List<Widget>? widgets;
   MyCustomAppBar({
     Key? key,
+    this.widgets,
     required this.titleText,
   }) : super(
           key: key,
           actions: [
             if (titleText != null) const HomeViewToggle(),
             const SettingsBtn(),
+            if (widgets != null && widgets.isNotEmpty) ...widgets,
           ],
           title: Text(
             titleText ?? "Dota2",

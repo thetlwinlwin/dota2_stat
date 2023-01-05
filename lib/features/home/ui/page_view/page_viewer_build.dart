59ca564dcd9563d323226780024bc641d4e138a8
thetlwinlwin <minthantlwin8@gmail.com>
2023-01-05 20:56:13 +0630

    scroll config for web

--- lib/features/home/ui/page_view/page_viewer_build.dart
+++ lib/features/home/ui/page_view/page_viewer_build.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -51,52 +53,67 @@ class _PageViewerState extends ConsumerState<PageViewer> {
           child: Center(
             child: SizedBox(
               height: 200,
-              child: PageView.builder(
-                controller: _topController,
-                onPageChanged: (value) {
-                  ref.read(pageIndexProvider.notifier).state = value;
-                  setState(() {
-                    _index = value;
-                  });
-                },
-                itemBuilder: (_, index) {
-                  Matrix4 matrix = Matrix4.identity();
-                  if (_currentVal.floor() == index) {
-                    final currScale =
-                        1 - (_currentVal - index) * (1 - _scaleFactor);
+              child: ScrollConfiguration(
+                behavior: ScrollConfiguration.of(context).copyWith(
+                  dragDevices: {
+                    PointerDeviceKind.touch,
+                    PointerDeviceKind.mouse,
+                  },
+                ),
+                child: PageView.builder(
+                  controller: _topController,
+                  onPageChanged: (value) {
+                    ref.read(pageIndexProvider.notifier).state = value;
+                    setState(() {
+                      _index = value;
+                    });
+                  },
+                  itemBuilder: (_, index) {
+                    Matrix4 matrix = Matrix4.identity();
+                    if (_currentVal.floor() == index) {
+                      final currScale =
+                          1 - (_currentVal - index) * (1 - _scaleFactor);
 
-                    matrix = Matrix4.diagonal3Values(1.0, currScale, 1);
-                  } else if (_currentVal.floor() + 1 == index) {
-                    final currScale = _scaleFactor +
-                        (_currentVal - index + 1) * (1 - _scaleFactor);
-                    matrix = Matrix4.diagonal3Values(1.0, currScale, 1);
-                  } else if (_currentVal.floor() - 1 == index) {
-                    matrix = Matrix4.diagonal3Values(1.0, _scaleFactor, 1);
-                  } else {
-                    matrix = Matrix4.diagonal3Values(1.0, _scaleFactor, 1);
-                  }
+                      matrix = Matrix4.diagonal3Values(1.0, currScale, 1);
+                    } else if (_currentVal.floor() + 1 == index) {
+                      final currScale = _scaleFactor +
+                          (_currentVal - index + 1) * (1 - _scaleFactor);
+                      matrix = Matrix4.diagonal3Values(1.0, currScale, 1);
+                    } else if (_currentVal.floor() - 1 == index) {
+                      matrix = Matrix4.diagonal3Values(1.0, _scaleFactor, 1);
+                    } else {
+                      matrix = Matrix4.diagonal3Values(1.0, _scaleFactor, 1);
+                    }
 
-                  return Transform(
-                    transform: matrix,
-                    child: PageViewTopChild(
-                      imgUrl: widget.stats[index].img,
-                    ),
-                  );
-                },
-                itemCount: widget.stats.length,
+                    return Transform(
+                      transform: matrix,
+                      child: PageViewTopChild(
+                        imgUrl: widget.stats[index].img,
+                      ),
+                    );
+                  },
+                  itemCount: widget.stats.length,
+                ),
               ),
             ),
           ),
         ),
         Expanded(
-          child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 300),
-            child: NameTag(
-              key: ValueKey(_index),
-              name: widget.stats[_index].name,
-              attribute: widget.stats[_index].attribute,
-              iconUrl: widget.stats[_index].icon,
-            ),
+          child: Column(
+            children: [
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                child: NameTag(
+                  key: ValueKey(_index),
+                  name: widget.stats[_index].name,
+                  attribute: widget.stats[_index].attribute,
+                  iconUrl: widget.stats[_index].icon,
+                ),
+              ),
+              Text(
+                '${widget.stats[_index].winRate.toStringAsPrecision(3)}%',
+              ),
+            ],
           ),
         ),
       ],
